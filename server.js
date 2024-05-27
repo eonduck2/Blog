671ca0494a2ce796d2312ea14577a8afe344c571
eonduck2 <whdtnxd@gmail.com>
2024-05-27 12:05:21 +0900

    프로젝트 초기화 && 기본 서버

---
+++ server.js
@@ -0,0 +1,38 @@
+const http = require(`node:http`);
+const fs = require(`node:fs`);
+
+const readFiles = (req, res, path, contentType) => {
+  fs.readFile(path, (err, data) => {
+    if (err != null) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end(data);
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    }
+  });
+};
+
+http
+  .createServer((req, res) => {
+    let url = req.url;
+    let fileExtension = url.split(`.`)[1];
+
+    console.log(`요청 url: ${url}`);
+    console.log(`지정 경로: ${url.slice(1, url.length)}`);
+
+    if (url === `/`) {
+      readFiles(req, res, `index.html`, `text/html`);
+    } else if (fileExtension === `ico`) {
+      readFiles(req, res, url.slice(1, url.length), `image/vnd.microsoft.icon`);
+    } else if (fileExtension === `css`) {
+      readFiles(req, res, url.slice(1, url.length), `text/css`);
+    } else if (fileExtension === `js`) {
+      readFiles(req, res, url.slice(1, url.length), `text/javascript`);
+    } else if (fileExtension === `html`) {
+      readFiles(req, res, url.slice(1, url.length), `text/html`);
+    }
+  })
+  .listen(8000, () => {
+    console.log(`the server is processing on http://localhost:8000`);
+  });
