3d05c9e2c58da8807acc7925411bc44e3fe73a7c
eonduck2 <whdtnxd@gmail.com>
2024-05-28 14:23:16 +0900

    테일윈드 관련 파일 삭제

--- src/styles/styles.css.ts
+++ src/styles/styles.css.ts
@@ -1,18 +1,35 @@
-import { createTheme, style } from "@vanilla-extract/css";
+// import { createTheme, style } from "@vanilla-extract/css";
 
-export const [themeClass, vars] = createTheme({
-  color: {
-    brand: "blue",
-    white: "#fff",
-  },
-  space: {
-    small: "4px",
-    medium: "8px",
-  },
-});
+// export const [themeClass, vars] = createTheme({
+//   color: {
+//     brand: "blue",
+//     white: "#fff",
+//   },
+//   space: {
+//     small: "4px",
+//     medium: "8px",
+//   },
+// });
 
-export const hero = style({
-  backgroundColor: vars.color.brandd,
-  color: vars.color.white,
-  padding: vars.space.large,
+// export const hero = style({
+//   backgroundColor: vars.color.brandd,
+//   color: vars.color.white,
+//   padding: vars.space.large,
+// });
+
+// import { style, globalStyle } from "@vanilla-extract/css";
+
+// export const myStyle = style({
+//   display: "flex",
+//   paddingTop: "3px",
+// });
+
+// globalStyle("body", {
+//   margin: 0,
+// });
+
+import { style } from "@vanilla-extract/css";
+
+export const container = style({
+  padding: 10,
 });
