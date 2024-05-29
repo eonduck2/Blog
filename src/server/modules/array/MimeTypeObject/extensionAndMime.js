f48c727884ab4653c80d212e11e35fbbe5008d54
eonduck2 <whdtnxd@gmail.com>
2024-05-29 23:03:08 +0900

    craete a moudle 'extensionAndMime.js'

---
+++ src/server/modules/array/MimeTypeObject/extensionAndMime.js
@@ -0,0 +1,9 @@
+const extensionsAndMime = {
+  "/": `text/html`,
+  ico: `image/vnd.microsoft.icon`,
+  css: `text/css`,
+  js: `text/javascript`,
+  html: `text/html`,
+};
+
+module.exports = extensionsAndMime;
--- src/server/modules/checking/checkTypes.js
+++ src/server/modules/checking/checkTypes.js
@@ -3,32 +3,32 @@ const checkTypes = (req) => {
   let fileExtension = url.split(`.`)[1];
   let path = url.slice(1, url.length);
 
-  if (url === `/`) {
-    return {
-      path: `./public/index.html`,
-      contentType: `text/html`,
-    };
-  } else if (fileExtension === `ico`) {
-    return {
-      path: path,
-      contentType: `image/vnd.microsoft.icon`,
-    };
-  } else if (fileExtension === `css`) {
-    return {
-      path: path,
-      contentType: `text/css`,
-    };
-  } else if (fileExtension === `js` || fileExtension === `mjs`) {
-    return {
-      path: path,
-      contentType: `text/javascript`,
-    };
-  } else if (fileExtension === `html`) {
-    return {
-      path: path,
-      contentType: `text/html`,
-    };
-  }
+  // if (url === `/`) {
+  //   return {
+  //     path: `./public/index.html`,
+  //     contentType: `text/html`,
+  //   };
+  // } else if (fileExtension === `ico`) {
+  //   return {
+  //     path: path,
+  //     contentType: `image/vnd.microsoft.icon`,
+  //   };
+  // } else if (fileExtension === `css`) {
+  //   return {
+  //     path: path,
+  //     contentType: `text/css`,
+  //   };
+  // } else if (fileExtension === `js` || fileExtension === `mjs`) {
+  //   return {
+  //     path: path,
+  //     contentType: `text/javascript`,
+  //   };
+  // } else if (fileExtension === `html`) {
+  //   return {
+  //     path: path,
+  //     contentType: `text/html`,
+  //   };
+  // }
 };
 
 module.exports = checkTypes;
