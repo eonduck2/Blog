72ddb387e9ee1a7aaec008859a3be21259a85123
eonduck2 <whdtnxd@gmail.com>
2024-05-31 09:25:01 +0900

    implemented 'readDirs.js'

---
+++ src/server/modules/reading/readDirs.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+
+/**
+ * TYPE[FUNCTION]
+ *
+ * STATUS[O] - 현재 상태
+ *
+ * CHECKLIST: 24.05.31/09:20 AM 구현
+ *
+ * NOTE: 1. 특정 폴더를 배열로 리턴해주는 함수
+ * @param path 폴더를 읽을 경로
+ */
+
+export default (path) => {
+  fs.readdir(path, (err, fileList) => {
+    if (err) {
+      throw new Error(`디렉토리 리딩 에러`);
+    }
+    return fileList;
+  });
+};
