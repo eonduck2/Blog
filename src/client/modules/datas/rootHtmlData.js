aedaec546e3c07ecc55a70f663c13ed619572c65
eonduck2 <whdtnxd@gmail.com>
2024-06-01 13:06:15 +0900

    update form path

--- src/client/modules/datas/rootHtmlData.js
+++ src/client/modules/datas/rootHtmlData.js
@@ -1,3 +1,5 @@
+import jsonDirList from "../array/createdJsonDirList.js";
+
 /**
  * TYPE[OBJECT]
  *
@@ -9,41 +11,81 @@
  *
  */
 
+// export default Object = {
+//   rootHtml: {
+//     rootHtmlData: `
+//     <div id="modal">
+//       <div id="x-icon">
+//         <i class="fas fa-x"></i>
+//       </div>
+//       <form action="/" method="post">
+//         <input type="text" name="title" id="" />
+//         <textarea name="content" id="" cols="30" rows="10"></textarea>
+//         <button type="submit">작성하기</button>
+//       </form>
+//     </div>
+
+//     <div>
+//       <div class="w-8 h-1/4 bg-pink-400">
+//           <header class="w-24 h-52">
+//               <div id="input-box" class="w-full h-full">
+//                   <button id="posting-button" class="bg-zinc-300">팝업</button>
+//               </div>
+//           </header>
+//       </div>
+//     <main>
+//       <aside id="postings" class="">
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//       </aside>
+//       <section>
+//         <div></div>
+//         <div></div>
+//       </section>
+//     </main>
+//   </div>
+//     `,
+//   },
+// };
 export default Object = {
   rootHtml: {
     rootHtmlData: `
-    <div id="modal">
-      <div id="x-icon">
-        <i class="fas fa-x"></i>
-      </div>
-      <form action="/" method="post">
-        <input type="text" name="title" id="" />
-        <textarea name="content" id="" cols="30" rows="10"></textarea>
-        <button type="submit">작성하기</button>
-      </form>
-    </div>
-
-    <div>
-      <div class="w-8 h-1/4 bg-pink-400">
-          <header class="w-24 h-52">
-              <div id="input-box" class="w-full h-full">
-                  <button id="posting-button" class="bg-zinc-300">팝업</button>
-              </div>
-          </header>
-      </div>
-    <main>
-      <aside id="postings" class="">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </aside>
-      <section>
-        <div></div>
-        <div></div>
-      </section>
-    </main>
-  </div>
-    `,
+        <div id="modal">
+          <div id="x-icon">
+            <i class="fas fa-x"></i>
+          </div>
+          <form action="/" method="post">
+            <input type="text" name="title" id="" />
+            <textarea name="content" id="" cols="30" rows="10"></textarea>
+            <button type="submit">작성하기</button>
+          </form>
+        </div>
+    
+        <div>
+          <div class="w-8 h-1/4 bg-pink-400">
+              <header class="w-24 h-52">
+                  <div id="input-box" class="w-full h-full">
+                      <button id="posting-button" class="bg-zinc-300">팝업</button>
+                  </div>
+              </header>
+          </div>
+        <main>
+          <aside id="postings" class="">
+            <ul>
+              ${jsonDirList
+                .map((fileName) => {
+                  return `<li>${fileName.substr(10).replace(".json", "")}</li>`;
+                })
+                .join("")}
+            </ul>
+          </aside>
+          <section>
+            <div></div>
+            <div></div>
+          </section>
+        </main>
+      </div>`,
   },
 };
