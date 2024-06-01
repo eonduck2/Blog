ffd2eeaf759dbcbe6b540ce50eb14c5abf90a77f
eonduck2 <whdtnxd@gmail.com>
2024-06-01 14:47:49 +0900

    drop file 'posted.html'

--- src/client/modules/datas/rootHtmlData.js
+++ src/client/modules/datas/rootHtmlData.js
@@ -11,44 +11,6 @@ import jsonDirList from "../array/createdJsonDirList.js";
  *
  */
 
-// export default Object = {
-//   rootHtml: {
-//     rootHtmlData: `
-//     <div id="modal">
-//       <div id="x-icon">
-//         <i class="fas fa-x"></i>
-//       </div>
-//       <form action="/" method="post">
-//         <input type="text" name="title" id="" />
-//         <textarea name="content" id="" cols="30" rows="10"></textarea>
-//         <button type="submit">작성하기</button>
-//       </form>
-//     </div>
-
-//     <div>
-//       <div class="w-8 h-1/4 bg-pink-400">
-//           <header class="w-24 h-52">
-//               <div id="input-box" class="w-full h-full">
-//                   <button id="posting-button" class="bg-zinc-300">팝업</button>
-//               </div>
-//           </header>
-//       </div>
-//     <main>
-//       <aside id="postings" class="">
-//         <div></div>
-//         <div></div>
-//         <div></div>
-//         <div></div>
-//       </aside>
-//       <section>
-//         <div></div>
-//         <div></div>
-//       </section>
-//     </main>
-//   </div>
-//     `,
-//   },
-// };
 export default Object = {
   rootHtml: {
     rootHtmlData: `
@@ -59,6 +21,7 @@ export default Object = {
           <form action="/" method="post">
             <input type="text" name="title" id="" />
             <textarea name="content" id="" cols="30" rows="10"></textarea>
+            
             <button type="submit">작성하기</button>
           </form>
         </div>
