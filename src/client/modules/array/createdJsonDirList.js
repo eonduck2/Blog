228224c1a46ea4a6f845c508581e773a2b25bff3
eonduck2 <whdtnxd@gmail.com>
2024-06-02 11:56:03 +0900

    'normalize.css'cdn is linked in 'index.html'

--- src/client/modules/array/createdJsonDirList.js
+++ src/client/modules/array/createdJsonDirList.js
@@ -1,14 +1,8 @@
 export default [
   "7118586641ㅂㅈㄷㄱㅇㄹㄴㅁ.json",
   "7212534401werfda.json",
-  "7231956101qweardfs.json",
-  "7231959560qweardfs.json",
-  "7231961207qweardfs.json",
-  "7231961320qweardfs.json",
-  "7231964775qweasdf.json",
-  "7231967872asdcz.json",
-  "7231968741asdcz.json",
-  "7231969427asdcz.json",
-  "7231969964asdcz.json",
-  "7231992579asdcz.json"
+  "7254786030weqrdfs.json",
+  "7254791850테스트.json",
+  "7254797199테스트.json",
+  "7254801827ㅂㅈㄷㅁㅇ.json"
 ]
