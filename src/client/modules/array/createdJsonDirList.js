8ec39d777f196dfa2f9abc7eaad134b37bb5be21
eonduck2 <whdtnxd@gmail.com>
2024-06-04 18:11:39 +0900

    update files

--- src/client/modules/array/createdJsonDirList.js
+++ src/client/modules/array/createdJsonDirList.js
@@ -1,9 +1,7 @@
 export default [
   "7118586641ㅂㅈㄷㄱㅇㄹㄴㅁ.json",
   "7212534401werfda.json",
-  "7334613462weqradfsweqrdasfweqrdrwqrwqrwqdfwqdfwqdfwq.json",
-  "7335333689weqradfsweqrdasfweqrdrwqrwqrwqdfwqdfwqdfwq.json",
-  "7335844163weqradfsweqrdasfweqrdrwqrwqrwqdfwqdfwqdfwq.json",
-  "7335870606weqradfsweqrdasfweqrdrwqrwqrwqdfwqdfwqdfwq.json",
-  "7335881371weqradfsweqrdasfweqrdrwqrwqrwqdfwqdfwqdfwq.json"
+  "7491778278테스트입니다..json",
+  "7491780304테스트입니다..json",
+  "7492272085ㅂㅈㄱㄷㅇㄻ.json"
 ]
