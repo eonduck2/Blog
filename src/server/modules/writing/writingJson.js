8e68e4fb52762ad60402e87bd21dea26a145f76c
eonduck2 <whdtnxd@gmail.com>
2024-06-05 17:47:28 +0900

    update 'json' folder

--- src/server/modules/writing/writingJson.js
+++ src/server/modules/writing/writingJson.js
@@ -17,8 +17,8 @@ export default (obj) => {
   const jsonParsedObj = JSON.stringify(obj, null, 2);
 
   fs.writeFile(
-    `./src/json/${id.uniqueId()}${fileName}.json`,
-    jsonParsedObj,
+    `./src/json/${id.uniqueId()}${fileName}.js`,
+    `export default ${jsonParsedObj}`,
     (err) => {
       if (err) {
         throw new Error(`제이슨 라이팅 에러`);
