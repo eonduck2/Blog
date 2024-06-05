92bc0769c19c6a1abae4b64261de80fd297bef79
eonduck2 <whdtnxd@gmail.com>
2024-06-05 14:57:52 +0900

    update comment 'clickForLists.js'

--- src/client/modules/event/clickForLists.js
+++ src/client/modules/event/clickForLists.js
@@ -1,3 +1,13 @@
+/** 
+ * TYPE[FUNCTION]
+ * 
+ * STATUS[O] - 현재 상태
+ * 
+ * CHECKLIST: 24.06.05
+ * 
+ * NOTE: 1. 작성 글 리스트에 컨텐츠를 나타내기 위한 클릭 이벤트 추가
+  @param NoParam
+*/
 export default () => {
   document.querySelectorAll(`li`).forEach((element) => {
     element.onclick = () => {
